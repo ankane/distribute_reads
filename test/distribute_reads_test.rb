f0da7238a68582d9cbc0c6244908c28e92f8bdc8
Andrew Kane <acekane1@gmail.com>
2017-03-29 00:53:52 -0700

    Test caching

--- test/distribute_reads_test.rb
+++ test/distribute_reads_test.rb
@@ -3,16 +3,21 @@ require_relative "test_helper"
 class DistributeReadsTest < Minitest::Test
   def setup
     # reset context
+    Makara::Cache.store = :memory
     Makara::Context.set_current(Makara::Context.generate)
   end
 
   def test_default
     assert_primary
+    assert_cache_size 0
   end
 
   def test_default_to_primary
     DistributeReads.default_to_primary = false
     assert_replica
+    insert_value
+    assert_primary
+    assert_cache_size 1
   ensure
     DistributeReads.default_to_primary = true
   end
@@ -25,6 +30,7 @@ class DistributeReadsTest < Minitest::Test
       insert_value
       assert_replica
     end
+    assert_cache_size 0
   end
 
   def test_distribute_reads_transaction
@@ -33,6 +39,7 @@ class DistributeReadsTest < Minitest::Test
         assert_primary
       end
     end
+    assert_cache_size 0
   end
 
   def test_max_lag
@@ -67,5 +74,9 @@ class DistributeReadsTest < Minitest::Test
 
   def assert_replica
     assert_equal "replica", current_database
+  end
+
+  def assert_cache_size(value)
+    assert_equal value, Makara::Cache.send(:store).instance_variable_get(:@data).size
   end
 end
