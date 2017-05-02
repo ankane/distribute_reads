663dc6c130d0ad71ff2565132546bfdfb7aa5841
Andrew Kane <acekane1@gmail.com>
2017-05-01 20:58:06 -0700

    Cleaner max lag code

--- lib/distribute_reads/global_methods.rb
+++ lib/distribute_reads/global_methods.rb
@@ -1,11 +1,12 @@
 module DistributeReads
   module GlobalMethods
     def distribute_reads(max_lag: nil)
+      if max_lag && DistributeReads.lag > max_lag
+        raise DistributeReads::TooMuchLag, "Replica lag over #{max_lag} seconds"
+      end
+
       previous_value = Thread.current[:distribute_reads]
       begin
-        if max_lag && DistributeReads.lag > max_lag
-          raise DistributeReads::TooMuchLag, "Replica lag over #{max_lag} seconds"
-        end
         Thread.current[:distribute_reads] = true
         yield
       ensure
