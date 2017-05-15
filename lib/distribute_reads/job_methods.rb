acfe3d2e426245e3fc72f94b527dbd488c39693c
Andrew Kane <acekane1@gmail.com>
2017-05-14 19:58:09 -0700

    Pass max_lag in jobs

--- lib/distribute_reads/job_methods.rb
+++ lib/distribute_reads/job_methods.rb
@@ -1,8 +1,8 @@
 module DistributeReads
   module JobMethods
-    def distribute_reads
+    def distribute_reads(max_lag: nil)
       around_perform do |job, block|
-        distribute_reads { block.call }
+        distribute_reads(max_lag: max_lag) { block.call }
       end
     end
   end
