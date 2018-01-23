e1e64a11dfa77f9979b32c2d83c5daa3a992dc04
Andrew <acekane1@gmail.com>
2018-01-23 02:35:30 -0800

    Break when lag failover triggered to remove potentially unnecessary queries

--- lib/distribute_reads/global_methods.rb
+++ lib/distribute_reads/global_methods.rb
@@ -25,6 +25,7 @@ module DistributeReads
                 # TODO possibly per connection
                 Thread.current[:distribute_reads][:primary] = true
                 Thread.current[:distribute_reads][:replica] = false
+                break
               else
                 raise DistributeReads::TooMuchLag, "Replica lag over #{max_lag} seconds#{options[:lag_on] ? " on #{base_model.name} connection" : ""}"
               end
