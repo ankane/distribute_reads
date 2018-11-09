33d8b48f25cae86e01f1575d5250b00f6663cedb
Marco Montagna <marcojoemontagna@gmail.com>
2018-11-08 23:16:11 -0800

    Prefix failover log messages with "[distribute_reads]".

--- lib/distribute_reads/appropriate_pool.rb
+++ lib/distribute_reads/appropriate_pool.rb
@@ -5,14 +5,14 @@ module DistributeReads
         if Thread.current[:distribute_reads][:replica]
           if @slave_pool.completely_blacklisted?
             raise DistributeReads::NoReplicasAvailable, "No replicas available" if Thread.current[:distribute_reads][:failover] == false
-            warn "No replicas available. Falling back to master pool."
+            warn "[distribute_reads] No replicas available. Falling back to master pool."
             @master_pool
           else
             @slave_pool
           end
         elsif Thread.current[:distribute_reads][:primary] || needs_master?(*args) || (blacklisted = @slave_pool.completely_blacklisted?)
           raise DistributeReads::NoReplicasAvailable, "No replicas available" if blacklisted && Thread.current[:distribute_reads][:failover] == false
-          warn "No replicas available. Falling back to master pool." if blacklisted
+          warn "[distribute_reads] No replicas available. Falling back to master pool." if blacklisted
           stick_to_master(*args) if DistributeReads.by_default
           @master_pool
         elsif in_transaction?
